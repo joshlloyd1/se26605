9891742d4268b496c63daa80a6274847f3f41360
Josh Lloyd <jmlloyd@email.neit.edu>
2017-10-16 09:22:18 -0400

    Get dogs as table

--- wk3demo/index.php
+++ wk3demo/index.php
@@ -1,6 +1,10 @@
 
 
 <?php
+require_once("assets/dbconn.php");
+require_once("assets/dogs.php");
 include_once("assets/header.php");
+$db = dbConn();
+echo getdogsAsTable($db);
 include_once("assets/footer.php");
 ?>
