6f39a8e8a32c68c739107e84a7e753de64128302
Josh Lloyd <jmlloyd@email.neit.edu>
2017-10-16 08:20:50 -0400

    Basic template

---
+++ wk3demo/assets/footer.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 001386538
+ * Date: 10/16/2017
+ * Time: 8:14 AM
+ */?>
+</section>
+<footer>
+    Copyright &copy; <?php echo date('Y'); ?> Josh Lloyd. All rights reserved.
+</footer>
+</body>
+</html>
---
+++ wk3demo/index.php
@@ -0,0 +1,6 @@
+
+
+<?php
+include_once("assets/header.php");
+include_once("assets/footer.php");
+?>
