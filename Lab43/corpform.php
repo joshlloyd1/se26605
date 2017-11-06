b3627aa4546ba6ec07201395c73c79ef6ee539c9
joshlloyd1 <32457132+joshlloyd1@users.noreply.github.com>
2017-11-05 19:13:18 -0500

    Lab 4 version 3

---
+++ Lab43/corpform.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 001386538
+ * Date: 10/23/2017
+ * Time: 8:12 AM
+ */
+// FORM FOR USERS TO ADD INFO INTO DATABASE
+
+// THIS IS OLD FORM
+?>
+<form method="post" action="#">
+    Corp: <input type="text" name="corp" value="" /> <br>
+    Email: <input type="text" name="email" value="" /> <br>
+    Zip Code: <input type="text" name="zipcode" value="" /> <br>
+    Owner: <input type="text" name="owner" value="" /> <br>
+    Phone number: <input type="text" name="phone" value="" /> <br>
+    <input type="submit" id="foo" name="action" value="Submit" />
+</form>
