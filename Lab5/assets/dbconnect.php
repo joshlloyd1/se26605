85d97ccf15b32f1bdecb9d1a906b27f439d8beb7
joshlloyd1 <32457132+joshlloyd1@users.noreply.github.com>
2017-11-27 10:12:16 -0500

    fix

---
+++ Lab5/assets/dbconnect.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 001386538
+ * Date: 11/8/2017
+ * Time: 10:34 AM
+ */
+function dbconnect() // DB CONNECT FUNCTION
+{
+    $dsn = "mysql:host=localhost;dbname=PHPClassFall2017";
+    $username = "root";
+    $password = "";
+    try {
+        $db = new PDO($dsn, $username, $password);
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+        return $db;
+    } catch (PDOException $e) { // IF THERE WAS A PROBLEM OUTPUTS PROBLEM
+        die("There was a problem connecting to the db.");
+    }
+}
